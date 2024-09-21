d8afbffefa5e51a68fbbd5555d3820a8d023a5cd
Amro <amroqadadha@gmail.com>
2024-09-21 17:38:12 +0300

    Add data parser factory

---
+++ RealTimeWeatherMonitoringService/DataParser/DataParserFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace RealTimeWeatherMonitoringService.DataParser
+{
+    public class DataParserFactory
+    {
+        public IDataParser GetParser(Formats format)
+        {
+            return format switch
+            {
+                Formats.Json => new JsonParser(),
+                Formats.Xml => new XmlParser(),
+                _ => throw new ArgumentException("Unsupported format")
+            };
+        }
+    }
+
+}
