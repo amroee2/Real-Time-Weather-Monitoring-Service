dde9f9d454257c455d99bb99dcd0a5510fb9065a
Amro <amroqadadha@gmail.com>
2024-09-23 13:24:11 +0300

    Add User Interface

---
+++ RealTimeWeatherMonitoringService/UserInterface.cs
@@ -0,0 +1,19 @@
+﻿namespace RealTimeWeatherMonitoringService
+{
+    public class UserInterface
+    {
+        public int GetUserType()
+        {
+            Console.WriteLine("Enter weather data");
+            Console.WriteLine("1- JSON\n2- XML");
+            int format = Convert.ToInt32(Console.ReadLine());
+            return format;
+        }
+        public string GetUserInput()
+        {
+            Console.WriteLine("Enter weather data");
+            string input = Console.ReadLine();
+            return input;
+        }
+    }
+}
