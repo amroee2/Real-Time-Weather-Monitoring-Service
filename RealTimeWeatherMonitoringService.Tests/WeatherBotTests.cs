2d6e3aed7b797a7429c1163f6e1d9631ff6e7a8c
Amro <amroqadadha@gmail.com>
2024-09-24 20:08:31 +0300

    Add tests for bots creation

--- RealTimeWeatherMonitoringService.Tests/WeatherBotTests.cs
+++ RealTimeWeatherMonitoringService.Tests/WeatherBotTests.cs
@@ -1,5 +1,6 @@
 ﻿using Moq;
 using RealTimeWeatherMonitoringService.Observer;
+using RealTimeWeatherMonitoringService.Settings;
 using RealTimeWeatherMonitoringService.WeatherBots;
 
 namespace RealTimeWeatherMonitoringService.Tests
@@ -36,6 +37,86 @@ namespace RealTimeWeatherMonitoringService.Tests
             weatherBot.Update(mockData.Object);
             mockContext.Verify(x => x.SetStrategy(weatherBot), Times.Once);
             mockContext.Verify(x => x.CheckThreshold(value), Times.Once);
+        }
+        [Theory]
+        [MemberData(nameof(GetBotTestData))]
+        public void ShouldCreateBot(string botType, BotData botData)
+        {
+            var factory = new WeatherBotFactory();
+
+            var bot = factory.CreateBot(botType, botData);
+
+            switch (botType)
+            {
+                case "RainBot":
+                    Assert.IsType<RainBot>(bot);
+                    break;
+                case "SnowBot":
+                    Assert.IsType<SnowBot>(bot);
+                    break;
+                case "SunBot":
+                    Assert.IsType<SunBot>(bot);
+                    break;
+                default:
+                    Assert.Fail($"Unknown bot type: {botType}");
+                    break;
+            }
+        }
+        public static IEnumerable<object[]> GetBotTestData()
+        {
+            yield return new object[]
+            {
+                "RainBot",
+                new BotData
+                {
+                    enabled = true,
+                    message = "RainBot activated",
+                    humidityThreshold = 80.0
+                }
+            };
+            yield return new object[]
+            {
+                "SnowBot",
+                new BotData
+                {
+                    enabled = true,
+                    message = "SnowBot activated",
+                    temperatureThreshold = -5.0
+                }
+            };
+            yield return new object[]
+            {
+                "SunBot",
+                new BotData
+                {
+                    enabled = true,
+                    message = "SunBot activated",
+                    temperatureThreshold = 35.0
+                }
+            };
+        }
+
+        [Fact]
+        public void ShouldRegisterBot()
+        {
+            var factory = new WeatherBotFactory();
+            factory.RegisterBot("RainBot", botData => new RainBot(botData.enabled, botData.message, botData.humidityThreshold));
+            factory.RegisterBot("SunBot", botData => new SunBot(botData.enabled, botData.message, botData.temperatureThreshold));
+            factory.RegisterBot("SnowBot", botData => new SnowBot(botData.enabled, botData.message, botData.temperatureThreshold));
+
+            Assert.Equal(3, WeatherBotFactory._botRegistry.Count);
+        }
+        [Fact]
+        public async void ShouldCreateAllBots()
+        {
+            Mock<IBotFactory> mockFactory = new Mock<IBotFactory>();
+            mockFactory.SetupSequence(x => x.CreateBot(It.IsAny<string>(), It.IsAny<BotData>()))
+                .Returns(new RainBot(true, "RainBot activated", 80.0))
+                .Returns(new SnowBot(true, "SnowBot activated", -5.0))
+                .Returns(new SunBot(true, "SunBot activated", 35.0));
+            BotsSettings botsSettings = new BotsSettings(mockFactory.Object);
+            List<WeatherBot<double>> weatherBots  = await botsSettings.ReadSettings();
+            Assert.Equal(3, weatherBots.Count);
         }
     }
 }
