5ce00e01ca00f49428bf35826098ac28bf1d4b8e
Amro <amroqadadha@gmail.com>
2024-09-24 15:01:35 +0300

    Add strategy pattern and weather bots tests

---
+++ RealTimeWeatherMonitoringService.Tests/WeatherBotStrategyTests.cs
@@ -0,0 +1,32 @@
+﻿using AutoFixture;
+using Moq;
+using RealTimeWeatherMonitoringService.WeatherBots;
+
+namespace RealTimeWeatherMonitoringService.Tests
+{
+    public class WeatherBotStrategyTests
+    {
+        [Fact]
+        public void ShouldSetStrategyCorrectly()
+        {
+            var weatherBotMock = new Mock<WeatherBot<double>>();
+            WeatherContext weatherContext = new WeatherContext();
+
+            weatherContext.SetStrategy(weatherBotMock.Object);
+
+            Assert.Equal(weatherBotMock.Object, weatherContext._weatherBot);
+        }
+        [Fact]
+        public void ShouldSetWeatherContextCorrectly() {
+            var weatherBotMock = new Mock<WeatherBot<double>>();
+            WeatherContext weatherContext = new WeatherContext();
+            weatherContext._weatherBot = weatherBotMock.Object;
+
+            Fixture fixture = new Fixture();
+            double value = fixture.Create<double>();
+            weatherContext.CheckThreshold(value);
+
+            weatherBotMock.Verify(w => w.CheckThreshold(It.IsAny<double>()), Times.Once);
+        }
+    }
+}
---
+++ RealTimeWeatherMonitoringService.Tests/WeatherBotTests.cs
@@ -0,0 +1,41 @@
+﻿using Moq;
+using RealTimeWeatherMonitoringService.Observer;
+using RealTimeWeatherMonitoringService.WeatherBots;
+
+namespace RealTimeWeatherMonitoringService.Tests
+{
+    public class WeatherBotTests
+    {
+        [Theory]
+        [InlineData("RainBot", 80.0)]
+        [InlineData("SunBot", 50.0)]
+        [InlineData("SnowBot", -1.0)]
+        public void ShouldUpdate(string BotType, double value)
+        {
+            WeatherBot<double> weatherBot;
+            Mock<IData> mockData = new Mock<IData>();
+            mockData.Setup(x => x.Humidity).Returns(value);
+            mockData.Setup(x => x.Temperature).Returns(value);
+            Mock<IContext> mockContext = new Mock<IContext>();
+            switch (BotType)
+            {
+                case "RainBot":
+                    weatherBot = new RainBot(true, "RainBot activated", 80.0);
+                    break;
+                case "SunBot":
+                    weatherBot = new SunBot(true, "SunBot activated", 50.0);
+                    break;
+                case "SnowBot":
+                    weatherBot = new SnowBot(true, "SnowBot activated", 0.0);
+                    break;
+                default:
+                    throw new ArgumentException("Unknown BotType");
+            }
+            mockContext.Setup(x => x.SetStrategy(weatherBot));
+            weatherBot.context = mockContext.Object;
+            weatherBot.Update(mockData.Object);
+            mockContext.Verify(x => x.SetStrategy(weatherBot), Times.Once);
+            mockContext.Verify(x => x.CheckThreshold(value), Times.Once);
+        }
+    }
+}
